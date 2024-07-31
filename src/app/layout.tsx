fdd26a858a8fb802abc3ff285d6311b9800b8e32
Stefan Feser <stefan@stefanfeser.com>
2024-07-31 15:20:52 +0200

    feat: add base layout styling | as / sf | DS-648

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
-import type { Metadata } from "next";
-import "./globals.scss";
+import type { Metadata } from 'next';
+import './globals.scss';
 import { getResetStyles } from '@stihl-design-system/components/partials';
-
+import styles from './layout.module.scss';
+import {
+  DSAriaLiveRegions,
+  DSHeading,
+  DSLinkStandalone,
+} from '@stihl-design-system/components';
 
 export const metadata: Metadata = {
-  title: "STIHL Quickstart Template",
-  description: "This repository offers a collection of basic page templates with our design system, providing ready-to-use layouts to ensure consistency and efficiency across projects.",
+  title: 'STIHL Quickstart Template',
+  description:
+    'This repository offers a collection of basic page templates with our design system, providing ready-to-use layouts to ensure consistency and efficiency across projects.',
 };
 
 export default function RootLayout({
@@ -14,9 +20,68 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>{getResetStyles()}</head>
-      <body>{children}</body>
+      <body>
+        {/* <DSSkipToContentLink /> */}
+        <DSAriaLiveRegions />
+        <main>{children}</main>
+        <footer className={styles.footer}>
+          <div className={styles['footer-content']}>
+            <div className='footer-col'>
+              <DSHeading tag='h2'>Heading 2</DSHeading>
+              <nav>
+                <ul>
+                  <li>
+                    <DSLinkStandalone href='#'>
+                      Standalone Link
+                    </DSLinkStandalone>
+                  </li>
+                  <li>
+                    <DSLinkStandalone href='#'>
+                      Standalone Link
+                    </DSLinkStandalone>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className='footer-col'>
+              <DSHeading tag='h2'>Heading 2</DSHeading>
+              <nav>
+                <ul>
+                  <li>
+                    <DSLinkStandalone href='#'>
+                      Standalone Link
+                    </DSLinkStandalone>
+                  </li>
+                  <li>
+                    <DSLinkStandalone href='#'>
+                      Standalone Link
+                    </DSLinkStandalone>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className='footer-col'>
+              <DSHeading tag='h2'>Heading 2</DSHeading>
+              <nav>
+                <ul>
+                  <li>
+                    <DSLinkStandalone href='#'>
+                      Standalone Link
+                    </DSLinkStandalone>
+                  </li>
+                  <li>
+                    <DSLinkStandalone href='#'>
+                      Standalone Link
+                    </DSLinkStandalone>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </footer>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,79 @@
-import styles from "./page.module.scss";
-import { DSButton } from "@stihl-design-system/components";
+import {
+  DSButton,
+  DSCheckbox,
+  DSFieldset,
+  DSHeading,
+  DSInput,
+  DSLogo,
+  DSRadioGroup,
+  DSText,
+  DSTitle,
+} from '@stihl-design-system/components';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
-    <main>
-      <DSButton>Click me</DSButton>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles['top-bar']}>
+          <div className={styles['top-bar-content']}>
+            <DSLogo href='/' />
+          </div>
+        </div>
+        <section className={styles['hero']}>
+          <div className={styles['hero-image']}>
+            <div className={styles['hero-image-title']}>
+              <DSHeading tag='h2'>Heading 2</DSHeading>
+            </div>
+          </div>
+        </section>
+      </header>
+      <section className={styles.content}>
+        <DSTitle className={styles.title} tag='h1'>
+          Heading 1
+        </DSTitle>
+
+        <form id='order-form' className={styles.form}>
+          <DSFieldset
+            legend={{
+              headingText: 'Heading 2',
+              headingTag: 'h2',
+              headingSize: 'large-uppercase',
+            }}
+            className={styles.fieldset}
+          >
+            <DSCheckbox name='business' label='I am a business client' />
+            <DSRadioGroup
+              legend='Title'
+              name='title'
+              alignment='horizontal'
+              options={[
+                { value: 'mr', label: 'Mr.' },
+                { value: 'mrs', label: 'Mrs.' },
+                { value: 'other', label: 'other' },
+              ]}
+              required
+            />
+            <DSInput
+              id='surname-input'
+              name='surname'
+              label='Surname'
+              required={true}
+              autoComplete='given-name'
+            />
+            <DSInput
+              id='name-input'
+              name='name'
+              label='Name'
+              required={true}
+              autoComplete='family-name'
+            />
+          </DSFieldset>
+          <DSButton className={styles['submit-button']} form='order-form'>
+            Button
+          </DSButton>
+        </form>
+      </section>
+    </>
   );
 }
