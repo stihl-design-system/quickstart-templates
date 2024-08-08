3cd7d05987fc535bf4ea69d56a817965d7652373
Anton Schmidt <anton.schmidt@onecode.de>
2024-08-08 11:07:25 +0200

    ci: add basePath | as

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   // GitHub Pages specific settings
   output: 'export',
+  // adjust GitHub Pages base path for favicon etc
+  basePath: '/quickstart-templates',
 };
 
 export default nextConfig;
