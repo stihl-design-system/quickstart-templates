47407b1693ede9182d151f6950839f7308b8ed9f
Stefan Feser <stefan@stefanfeser.com>
2024-08-09 11:15:25 +0200

    feat: set basePath to /quickstart-templates by default | sf

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,7 @@
 const nextConfig = {
   // GitHub Pages specific settings
   output: 'export',
+  basePath: '/quickstart-templates',
 };
 
-if (process.env.NODE_ENV === 'production') {
-  // adjust GitHub Pages base path for favicon etc
-  nextConfig.basePath = '/quickstart-templates';
-  nextConfig.assetPrefix = '/quickstart-templates/';
-}
 export default nextConfig;
--- playwright/axe.a11y.ts
+++ playwright/axe.a11y.ts
@@ -2,20 +2,22 @@ import AxeBuilder from '@axe-core/playwright';
 import { expect, test } from '@playwright/test';
 import { createHtmlReport } from 'axe-html-reporter';
 
-
-const PAGES_TO_TEST = [
-  '/',
-];
-
+const PAGES_TO_TEST = ['/quickstart-templates'];
 
 test.describe('Next.js App Accessibility Tests', () => {
   for (const pagePath of PAGES_TO_TEST) {
-    test(`Verify no accessibility issues on ${pagePath}`, async ({ page, browserName }) => {
+    test(`Verify no accessibility issues on ${pagePath}`, async ({
+      page,
+      browserName,
+    }) => {
       await page.goto(`http://localhost:3000${pagePath}`);
 
       const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
-      const reportFileName = `accessibility-report-${browserName}${pagePath.replace(/\//g, '-')}.html`;
+      const reportFileName = `accessibility-report-${browserName}${pagePath.replace(
+        /\//g,
+        '-'
+      )}.html`;
       createHtmlReport({
         results: accessibilityScanResults,
         options: {
@@ -24,7 +26,6 @@ test.describe('Next.js App Accessibility Tests', () => {
           reportFileName: reportFileName,
         },
       });
-
 
       expect(accessibilityScanResults.violations).toEqual([]);
     });
