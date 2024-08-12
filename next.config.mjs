9cf094ad54c365615c17026820ca6ce15896dc61
Stefan Feser <stefan@stefanfeser.com>
2024-08-12 10:36:34 +0200

    ci: use adjusted assetPrefix just for vrts | sf

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,11 @@ const nextConfig = {
   // GitHub Pages specific settings
   output: 'export',
   basePath: '/quickstart-templates',
-  assetPrefix: './',
-  trailingSlash: true,
 };
+
+if (process.env.NODE_ENV === 'test') {
+  nextConfig.assetPrefix = './';
+  nextConfig.trailingSlash = true;
+}
 
 export default nextConfig;
