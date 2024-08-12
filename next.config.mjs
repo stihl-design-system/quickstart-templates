f9d163ee6acce311370f10c6c2401e6bbc0b655a
Stefan Feser <stefan@stefanfeser.com>
2024-08-12 09:39:48 +0200

    Revert "ci: try adding assetPrefix | sf"
    This reverts commit 8e4318ca3325b5606c60e367ad91817da438b632.

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ const nextConfig = {
   // GitHub Pages specific settings
   output: 'export',
   basePath: '/quickstart-templates',
-  assetPrefix: './',
-  trailingSlash: true,
 };
 
 export default nextConfig;
