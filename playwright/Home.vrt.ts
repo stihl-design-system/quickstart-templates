a0e26771bdae51ca883f9f4d0289f6e76e7e2399
Stefan Feser <stefan@stefanfeser.com>
2024-08-12 11:06:57 +0200

    test: add VRTs for different viewports | sf

--- playwright/Home.vrt.ts
+++ playwright/Home.vrt.ts
@@ -23,6 +23,19 @@ interface ScreenshotOptions {
   timeout?: number;
 }
 
+type Options = {
+  scenario?: (page: Page) => Promise<void>;
+  customBreakpoints?: number[];
+  shouldUseExtendedBreakpoints?: boolean;
+  // Should be passed if executing multiple media query tests in one .vrt file, to make the tests distinguishable
+  testTitle?: string;
+  shouldSkipFFAndWebkitBrowser?: boolean;
+};
+
+const BREAKPOINTS = [319, 320, 768, 1024, 1536];
+const BREAKPOINTS_EXTENDED = [1668, 1920, 2560];
+const PAGE_URL = 'http://localhost:3000';
+
 /**
  * Take a screenshot with disabled animations and an YIQ color space threshold of 0.2
  * Read more about threshold here: https://playwright.dev/docs/api/class-pageassertions#page-assertions-to-have-screenshot-1-option-threshold
@@ -30,7 +43,7 @@ interface ScreenshotOptions {
  * @param title filename of screenshot
  * @param screenshotOptions {ScreenshotOptions} (optional) - Options object for the screenshot
  */
-export const takeScreenshot = async (
+const takeScreenshot = async (
   page: Page,
   title: string,
   screenshotOptions?: ScreenshotOptions
@@ -43,12 +56,60 @@ export const takeScreenshot = async (
   });
 };
 
-test('visual regression test for homepage', async ({ page }, { title }) => {
-  // Navigate to the page you want to test
-  await page.goto('http://localhost:3000', {
-    waitUntil: 'networkidle',
-  });
+/**
+ *
+ * @param pageId - The page ID where the VRT should be done
+ * @param options {Options} (optional) - Options object
+ * @param screenshotOptions {ScreenshotOptions} (optional) - Options object for the screenshot
+ *
+ * Executes media query tests for all available breakpoints.
+ * If executing multiple media query tests in one .vrt file
+ * make sure to set testTitle param in the options object.
+ *
+ * If customBreakpoints are passed, only those breakpoints will be tested.
+ * */
+const executeMediaQueryTests = async (
+  options?: Options,
+  screenshotOptions?: ScreenshotOptions
+): Promise<void> => {
+  const {
+    scenario,
+    shouldUseExtendedBreakpoints,
+    testTitle,
+    customBreakpoints,
+  } = options || {};
 
-  // Take a screenshot of the full page
-  await takeScreenshot(page, title, { fullPage: true });
+  let selectedBreakpoints = BREAKPOINTS;
+
+  // Custom breakpoints have priority over shouldUseExtendedBreakpoints
+  if (customBreakpoints) {
+    selectedBreakpoints = customBreakpoints;
+  } else if (shouldUseExtendedBreakpoints) {
+    selectedBreakpoints = [...BREAKPOINTS, ...BREAKPOINTS_EXTENDED];
+  }
+
+  selectedBreakpoints.forEach((breakpoint) => {
+    test(`${
+      testTitle ? testTitle + ' ' : ''
+    }viewport width ${breakpoint}px`, async ({ page }, { title }) => {
+      // Needs to be set for proper screen height
+      await page.setViewportSize({ width: breakpoint, height: 1 });
+
+      await page.goto(PAGE_URL, { waitUntil: 'networkidle' });
+      await page.setViewportSize({
+        width: breakpoint,
+        height: await page.evaluate(() => document.body.clientHeight),
+      });
+
+      if (scenario) {
+        await scenario(page);
+      }
+
+      await takeScreenshot(page, title, screenshotOptions);
+    });
+  });
+};
+
+test.describe('visual regression tests in different viewports', () => {
+  executeMediaQueryTests();
 });
