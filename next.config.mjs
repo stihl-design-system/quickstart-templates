8e4318ca3325b5606c60e367ad91817da438b632
Stefan Feser <stefan@stefanfeser.com>
2024-08-12 09:36:02 +0200

    ci: try adding assetPrefix | sf

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   // GitHub Pages specific settings
   output: 'export',
   basePath: '/quickstart-templates',
+  assetPrefix: './',
+  trailingSlash: true,
 };
 
 export default nextConfig;
