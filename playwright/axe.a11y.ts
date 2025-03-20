9b0c615eb52e7b9f7e2590ab346c5b4ff86a4e9d
Anton Schmidt <anton.schmidt@onecode.de>
2025-03-20 11:07:44 +0100

    test: use WCAG 2.2 AA rules in a11y test | as

--- playwright/axe.a11y.ts
+++ playwright/axe.a11y.ts
@@ -12,7 +12,23 @@ test.describe('Next.js App Accessibility Tests', () => {
     }) => {
       await page.goto(`http://localhost:3000${pagePath}`);
 
-      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .options({
+          runOnly: {
+            type: 'tag',
+            // rule descriptions https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md#experimental-rules
+            values: [
+              'wcag2a',
+              'wcag2aa',
+              'wcag21a',
+              'wcag21aa',
+              // Also add non-default WCAG 2.2 A and WCAG 2.2 AA to our tests
+              'wcag22a',
+              'wcag22aa',
+            ],
+          },
+        })
+        .analyze();
 
       const reportFileName = `accessibility-report-${browserName}${pagePath.replace(
         /\//g,
