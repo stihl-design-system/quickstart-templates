3258d64d5e6676160b57ef49abc88aec8fcc0864
Stefan Feser <stefan@stefanfeser.com>
2026-06-10 13:00:05 +0200

    deps: update playwright to latest version | sf

--- docker.sh
+++ docker.sh
@@ -3,4 +3,4 @@
 set -o errexit
 set -o pipefail
 
-docker run --rm --network host -v $(pwd):/work/ -v /work/node_modules -w /work/ -it playwright:v1.51.1-noble-vrt-quick-start "${@}"
+docker run --rm --network host -v $(pwd):/work/ -v /work/node_modules -w /work/ -it playwright:v1.60.0-noble-vrt-quick-start "${@}"
